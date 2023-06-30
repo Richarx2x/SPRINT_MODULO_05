70ff054daa9eaab77e2396b797724d8e7e50f31b
Aracily <71363174+aaracily@users.noreply.github.com>
2023-06-29 21:49:29 -0400

    Create ClteDTO.java

---
+++ src/main/java/cl/grupo05/model/dto/ClteDTO.java
@@ -0,0 +1,73 @@
+public class ClteDTO extends UserDTO {
+  private String rut;
+  private String apellido;
+   private String correo;
+   private String telefono;
+    private String afp;
+   // private int sistemaSalud; Evaluar si incorporarlo para este sprint
+    private String direccion;
+    private String comuna;
+   // private int edad;
+
+  public ClteDTO (){
+  }
+  public ClteDTO(String rut, String apellido,String correo,String telefono, String afp,String direccion, String comuna){
+    super(id,nombre,username.password);
+      this.rut = rut;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.afp = afp;
+        this.direccion = direccion;
+        this.comuna = comuna;
+  }
+
+   public String getRut() {
+        return rut;
+    }
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+   public String getCorreo() {
+        return rut;
+    }
+    public void setCorreo(String correo) {
+        this.rut = rut;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(String Telefono) {
+        this.telefono = telefono;
+    }
+    public String getAfp() {
+        return afp;
+    }
+
+    public void setAfp(String afp) {
+        this.afp = afp;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public String getComuna() {
+        return comuna;
+    }
+
+    public void setComuna(String comuna) {
+        this.comuna = comuna;
+    }
+
+}
