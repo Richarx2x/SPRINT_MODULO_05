5334319436afa353da7503096379316a095bba56
Aracily <71363174+aaracily@users.noreply.github.com>
2023-06-29 21:07:28 -0400

    Create AdminDTO.java

---
+++ src/main/java/cl/grupo05/model/dto/AdminDTO.java
@@ -0,0 +1,45 @@
+public class AdminDTO extends UserDTO {
+  private String run;
+  private String apellido;
+  private String correo;
+  private String area;
+
+  public AdminDTO(){
+  }
+  public AdminDTO(String run,String apellido,String correo,String area){
+    super(id,nombre,username,password);
+    this.run =run;
+    this.apellido = apellido;
+    this.correo = correo;
+    this.area = area;
+  }
+    public String getRun() {
+        return run;
+    }
+
+    public void setRut(String run) {
+        this.run = run;
+    }
+     public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+   public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo =  correo;
+    }
+   public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area =  area;
+    }
+
+}
